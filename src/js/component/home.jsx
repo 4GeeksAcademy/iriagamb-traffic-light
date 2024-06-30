30e978dd6c46e721dc49c2e2b7b7ed6f89eec908
Iriagamb <77029483+Iriagamb@users.noreply.github.com>
2024-06-30 05:54:29 +0000

    src

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import Traffic from "./traffic"
+
+import React, { useState } from "react";
+
 //include images into your bundle
 
 //create your first component
 const Home = () => {
+	const [selectColor, setSelecteColor] = useState("red");
 	return (
-		<div className="text-center">
-			<Traffic/>
-		</div>
-	);
+		<div className="Traffic-light">
+      <div className="light red glow"></div>
+      <div className="light yellow glow"></div>
+      <div className="light green glow"></div>
+      
+    </div>
+  );
+	
 };
 
 export default Home;
